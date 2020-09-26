32c9312cb99e73b13d8d5658c0d5edc62a9ef726
Romario Gustave <romagustave10@gmail.com>
2020-09-26 01:14:43 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
     def create
         @user = User.find_by(name: params[:user][:name])
-     
+
         if @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
-        else         
+        else
             return head(:forbidden)
             redirect_to action: "new"
         end
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
     end
 
     private
-  
+
     def user_params
       params.require(:user).permit(:name, :password, :password_confirmation)
     end
